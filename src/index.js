63f1d127ab86e3a04794f40eb3ca6403696aa14d
Afia246 <afiaadilah246@gmail.com>
2024-07-08 17:20:16 +0600

    frontend

--- src/index.js
+++ src/index.js
@@ -1,55 +1,16 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './components/Home';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'; // Import the CSS file
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const App = () => {
-  return (
-    <Router>
-      <div className="container mt-5">
-        <nav className="navbar navbar-expand-lg navbar-coffee">
-          <Link className="navbar-brand" to="/">
-            <img src="/images/logo.png" alt="App Logo" className="app-logo" /> {/* Use the correct path to your logo */}
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/login">Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/signup">Signup</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(<App />);
-
-export default App;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
